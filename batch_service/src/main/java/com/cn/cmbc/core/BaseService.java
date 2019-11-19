cd3770121a852de04ff1fcc0cb8c0e501a31c0ea
wangjiang <541478324@qq.com>
2019-11-19 20:24:15 +0800

    添加本地仓库

---
+++ batch_service/src/main/java/com/cn/cmbc/core/BaseService.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (C), 2015-2019, XXX有限公司
+ * FileName: BaseService
+ * Author:   wangjiang
+ * Date:     2019/11/15 23:28
+ * Description: 文件下载本地最为基础的类
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.cn.cmbc.core;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈文件下载本地最为基础的类〉
+ * 继承即可灵活获取数据，封装实体类
+ *
+ * @author wangjiang
+ * @create 2019/11/15
+ * @since 1.0.0
+ */
+public abstract class BaseService {
+
+
+}
---
+++ batch_service/src/main/java/com/cn/cmbc/core/XmlBaseService.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright (C), 2015-2019, XXX有限公司
+ * FileName: XmlBaseService
+ * Author:   wangjiang
+ * Date:     2019/11/15 23:45
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.cn.cmbc.core;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author wangjiang
+ * @create 2019/11/15
+ * @since 1.0.0
+ */
+public abstract class XmlBaseService extends BaseService{
+
+}
